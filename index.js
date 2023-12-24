52a9f9d5d18fb5e77c786eca27cbb4106d8ff33f
Irfann95 <irfann.assana@epitech.eu>
2023-12-24 14:59:50 +0100

    ajout du dossier

---
+++ index.js
@@ -0,0 +1,19 @@
+const navMenu = document.querySelector('.nav__menu')
+const navCloseBtn = document.querySelector('.nav__toggle-croix')
+const navOpenBtn = document.querySelector('.nav__toggle-open')
+
+const openNavHandler = () => {
+    navMenu.style.display = 'flex';
+    navOpenBtn.style.display='none'
+    navCloseBtn.style.display='inline-block'
+}
+
+const closeNavHandler = () => {
+    navMenu.style.display = 'none';
+    navOpenBtn.style.display='inline-block'
+    navCloseBtn.style.display='none'
+}
+
+
+navOpenBtn.addEventListener('click', openNavHandler)
+navCloseBtn.addEventListener('click', closeNavHandler)
